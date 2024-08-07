e7163dde2f1dd5703f15f2ee22e597de56f4a88d
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-07 09:11:24 -0600

    menú en la clase principal

--- Program.cs
+++ Program.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using Laboratorio_2;
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        SistemaReservas sistemaReservas = new SistemaReservas();
+        bool salir = false;
+
+        while (!salir)
+        {
+            Console.WriteLine("Menú:");
+            Console.WriteLine("1. Agregar Habitación");
+            Console.WriteLine("2. Eliminar Habitación");
+            Console.WriteLine("3. Mostrar Habitaciones");
+            Console.WriteLine("4. Asignar Habitación a Cliente");
+            Console.WriteLine("5. Liberar Habitación");
+            Console.WriteLine("6. Salir");
+            Console.Write("Seleccione una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    AgregarHabitacion(sistemaReservas);
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                   
+                    break;
+                case 4:
+                  
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    salir = true;
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        }
+    }
+
+    static void AgregarHabitacion(SistemaReservas sistemaReservas)
+    {
+        Console.WriteLine("Seleccione el tipo de habitación:");
+        Console.WriteLine("1. Habitación Simple");
+        Console.WriteLine("2. Habitación Doble");
+        Console.WriteLine("3. Suite");
+        Console.WriteLine("4. Habitación Deluxe");
+        int tipo = int.Parse(Console.ReadLine());
+
+        Console.Write("Número: ");
+        int numero = int.Parse(Console.ReadLine());
+        Console.Write("Precio por noche: ");
+        double precio = double.Parse(Console.ReadLine());
--- SistemaReservas.cs
+++ SistemaReservas.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Laboratorio_2
-{
-    internal class SistemaReservas
-    {
-    }
-}
+﻿
