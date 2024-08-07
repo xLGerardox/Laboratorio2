475a56a5311fb16e41b3ef4c6ec1e5c4b669a66e
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-07 09:09:53 -0600

    se crearon varias clases para el menu

---
+++ Habitacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Habitacion
+{
+    public int Numero { get; set; }
+    public double PrecioPorNoche { get; set; }
+    public bool Disponible { get; private set; }
+    public string ClienteAsignado { get; private set; }
+
+    public Habitacion(int numero, double precioPorNoche)
+    {
+        Numero = numero;
+        PrecioPorNoche = precioPorNoche;
+        Disponible = true;
+        ClienteAsignado = string.Empty;
+    }
